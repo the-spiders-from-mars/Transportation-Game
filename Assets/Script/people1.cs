2c435592e6a1e1724756ccd1f280210fc10e20d6
Luminoid <14302010006@fudan.edu.cn>
2017-04-23 14:34:46 +0800

    feat(Model): add passengers

---
+++ Assets/Script/people1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class people1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider collision){
+		Destroy (this.gameObject);
+	}
+}
